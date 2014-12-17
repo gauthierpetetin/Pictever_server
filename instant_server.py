6a9100d01b2b6a1dcb2d9f311241a187d56d27cf
Martin Charrel <martin.charrel@gmail.com>
2014-12-17 10:04:11 +0100

    debug true

--- instant_server.py
+++ instant_server.py
@@ -4,5 +4,5 @@ from instant_server.server import app
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     #debug = (port == 5000)
-    debug = False
+    debug = True
     app.run(host='0.0.0.0', port=port, debug=debug)
